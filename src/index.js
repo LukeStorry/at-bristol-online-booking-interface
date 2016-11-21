2b21a9c7390b4979ebac18d32a150ba52bac6029
Luke Storry <Luke@LukeStorry.co.uk>
2016-11-21 22:48:03 +0000

    started linking node to firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 var Router = require('react-router').Router;
 var Route = require('react-router').Route;
 var browserHistory = require('react-router').browserHistory;
+var admin = require("firebase-admin");
 
 var Hello = require('./app/hello');
 
@@ -14,3 +15,16 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
+
+//TODO: change database to media app
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: "spe-booking",
+    clientEmail: "booking-service@spe-booking.iam.gserviceaccount.com",
+    privateKey: "-----BEGIN PRIVATE KEY-----\n<KEY>\n-----END PRIVATE KEY-----\n"
+  }),
+  databaseURL: "https://spe-booking.firebaseio.com"
+});
+
+//NOTE: maybe set-up differently here as if a client with no access rights?
+  // - database would then have to be more open
