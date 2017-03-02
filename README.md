7f01f94cde30e93ece9553703e5df1ff59a80d36
begly <service.alex@begly.co.uk>
2017-03-02 19:08:15 +0000

    update documentaiton

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # SPE Booking
-
-Interface to add new bookings into Firebase
+Mock interface for adding bookings. This app can be used as either a home booking or a booking done via a terminal outside of @bristol. Bookings done via the terminal also go into the recent bookings collection which allows them to be shown to the front desk worker.
 
 ![](docs/interface.png)
 
-## Usage
-
+## Documention
+### Usage
 ```bash
 $ git clone https://github.com/begly/spe-booking.git
 $ cd spe-booking
@@ -15,34 +14,16 @@ $ npm start
 
 Now open [http://localhost:5000](http://localhost:5000).
 
+### Firebase interaction
+Uses the [spe-bookings firebase project](https://console.firebase.google.com/project/spe-booking).
 
+Writes bookings to booking collection and uses an event watcher to then add to the recent bookings collection. Bookings consists of documents of the form:
+```
+{
+  "email": "example@example.com",
+  "quantity": "2",
+  "timestamp": 1481721844976
+}
+```
 
-## Document TODO
-
-writes to the [spe-bookings firebase project](https://console.firebase.google.com/project/spe-booking)
-
-
-
-
-
-
-## TODO:
-
-* Start a Node.js project, working with the build tools I've mentioned
-* write the logic, you can use some of the code I've written but some needs to change
-* Also the code I've written doesn't use features from ECMAScript which we might want to use, and I think we should use
-* Write an interface for the server, use bootstrap, I think its a good idea
-* Write tests for the code
-* switch to spe firebase project database
-* Verification needs to be done server side, client side introduces security issues
-
-
-* `".write": "auth.uid === 'booking-service' && !data.exists()",`
-
-
-
-* maybe use bootstrap package - for server
-* Write a package for easy install
-* Use react to keep consistent, it is overkill but it makes the project more uniform
-* ECMAScript is used on exhibitions API so maybe we should use here, makes OO easier
-however requires compilation
+Recent bookings collection contains a list of IDs of recent bookings to be used by the front desk interface.
--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,33 @@
+/* Main booking logic
+ *
+ * Sign into page anonoymously, giving read access to booking & recent booking
+ * collections. Once user has entered booking info, grab from DOM, save to booking
+ * collection and use event listener watching bookings to add to recent bookings
+ * collection.
+ */
+
+
 var new_items = false;
 
 function App() {
-    this.user;
-    this.book_button = document.getElementById('book');
-    this.booking_quantity_input = document.getElementById('booking-quantity');
-    this.booking_email_input = document.getElementById('booking-email');
+    this.user; // Firebase authenticated user
 
+    // DOM elements
+    this.book_button            = document.getElementById('book');
+    this.booking_quantity_input = document.getElementById('booking-quantity');
+    this.booking_email_input    = document.getElementById('booking-email');
+
+    // Setup event listeners
     this.book_button.addEventListener('click', this.book.bind(this));
 
-    this.init_firebase();
-    //this.booking_ref.
-    this.booking_ref.once('child_added', function() {
-        
-    });
+    // Init firebase
+    this.init_firebase(); 
+
+    // Init firebase listeners
+    // NOTE firebase child added returns all children, new_items allows us to
+    // only retrive the latest by flushing the old ones and then adding a listener
     new_items = false;
+    this.booking_ref.once('child_added', function() {}); // Empty call to flush old children
     this.booking_ref.on('child_added', this.save_recent_booking.bind(this));
     this.booking_ref.once('child_added', function() {
         new_items = true;
@@ -23,26 +37,29 @@ function App() {
 // Sets up shortcuts to Firebase features
 App.prototype.init_firebase = function() {
     this.database = firebase.database();
-    this.storage = firebase.storage();
-    this.auth = firebase.auth()
+    this.storage  = firebase.storage();
+    this.auth     = firebase.auth()
 
-    this.booking_ref = this.database.ref('bookings');
+    this.booking_ref         = this.database.ref('bookings');
     this.recent_bookings_ref = this.database.ref('recent_bookings');
-    this.booking_ref.off();
+    this.booking_ref.off(); // Turn off any listeners 
 
     this.auth.onAuthStateChanged(this.on_auth_state_change.bind(this));
 };
 
+// Every page signs in anonymously, this gives read access to the user
 App.prototype.sign_in = function() {
     firebase.auth().signInAnonymously().catch(function(error) {
         console.error('Sign in failed', error);
     });
 }
 
+// Update the user attribute on state change
 App.prototype.on_auth_state_change = function(user) {
     this.user = user;
 }
 
+// Retrives booking info from DOM
 App.prototype.get_booking_input = function() {
     return {
         quantity: this.booking_quantity_input.value,
@@ -50,11 +67,13 @@ App.prototype.get_booking_input = function() {
     };
 }
 
+// Clear booking info from DOM
 App.prototype.clear_booking_input = function() {
     this.booking_quantity_input.value = this.booking_quantity_input.getAttribute('data-default');
     this.booking_email_input.value = this.booking_email_input.getAttribute('data-default');
 }
 
+// Take input data, save to database, clear fields
 App.prototype.book = function() {
     var booking_data = this.get_booking_input();
 
@@ -64,6 +83,7 @@ App.prototype.book = function() {
     this.clear_booking_input();
 }
 
+// Write booking data to DB
 App.prototype.save_booking = function(booking_data) {
     booking_data.timestamp = firebase.database.ServerValue.TIMESTAMP;
 
@@ -72,8 +92,9 @@ App.prototype.save_booking = function(booking_data) {
     });
 }
 
+// Write to recent booking collection 
 App.prototype.save_recent_booking = function(snapshot) {
-    if (!new_items) return;
+    if (!new_items) return; // We only want to write the new items
 
     var id = snapshot.key;
     this.database.ref('recent_bookings/' + id).set(id).catch(function(error) {
